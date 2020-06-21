3973851787456545dcde050153913aedc8868b91
YASH PRIYAM <yashpriyam@YASHs-MacBook-Air.local>
2020-06-21 23:07:45 +0530

    Updates

---
+++ precache-manifest.e2f309f13316da2b327bdc6838448a21.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "2dd4ad0db7c219c79e448870f854efb3",
+    "url": "/react-snake-game/index.html"
+  },
+  {
+    "revision": "6f4f4293425172fb4e16",
+    "url": "/react-snake-game/static/css/main.d414fbf9.chunk.css"
+  },
+  {
+    "revision": "03a06e6e5b9bcf8b2490",
+    "url": "/react-snake-game/static/js/2.4667a05b.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-snake-game/static/js/2.4667a05b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "6f4f4293425172fb4e16",
+    "url": "/react-snake-game/static/js/main.72f97579.chunk.js"
+  },
+  {
+    "revision": "8e4ba1d252d608b78a04",
+    "url": "/react-snake-game/static/js/runtime-main.af1f51f7.js"
+  }
+]);
